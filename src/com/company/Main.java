1ea6e10e3eb9353c8cda7c998fb7604f96ec810a
KerwinJT <42225159+KerwinJT@users.noreply.github.com>
2018-08-13 17:53:00 -0600

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,10 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-    SumOf5and3 summing = new SumOf5and3();
-    summing.summingOperatoin();
-    EvenFibonacciNumbers fibonacciSum = new EvenFibonacciNumbers();
-    fibonacciSum.evenFibonacciSummming();
+    SumOf5and3 summing = new SumOf5and3(); //Creates a new object of SumOf5and3
+    summing.summingOperatoin();		   //Calls method to sum the divisable numbers.
+    EvenFibonacciNumbers fibonacciSum = new EvenFibonacciNumbers(); //Creates a new object of EvenFibonacciNumbers.
+    fibonacciSum.evenFibonacciSummming();			    //Calls method evenFibonacciSumming.
 
 	// write your code here
     }
